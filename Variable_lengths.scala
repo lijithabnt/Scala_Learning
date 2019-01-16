c1316ee057aa8a4fa19ee0978dd1f9268499d145
Lijitha <43061766+lijithabnt@users.noreply.github.com>
2019-01-16 16:16:07 +0530

    Update Variable_lengths.scala

--- Variable_lengths.scala
+++ Variable_lengths.scala
@@ -11,3 +11,10 @@ val sum1 = (x:Int*) => x.sum1
 val sum2 = (list:List[Int]) => list.sum2
 //To convert a list as a argumet :
   =_*
+
+variabe arguments should be given at end
+val add1 = (x:Int,y:Int*) => x+y
+val add1 = (x:Int*,y:Int*) => x+y //This cannot be done so we go for currying
+because for first one we dont know how many arguments ar there and from where to take the second arguments list
+
+
