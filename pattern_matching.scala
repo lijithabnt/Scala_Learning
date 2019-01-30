7995ccf489c6094cf4bdb395532f1fc1b84e339e
Lijitha <43061766+lijithabnt@users.noreply.github.com>
2019-01-30 16:34:56 +0530

    Create pattern_matching.scala

---
+++ pattern_matching.scala
@@ -0,0 +1,44 @@
+constant pattern matching :
+=========================
+
+import scala.util.Random
+
+val x: Int = Random.nextInt(8)
+
+x match {
+  case 0 => "zero"
+  case 1 => "one"
+  case 2 => "two"
+  case _ => "many"
+}
+
+//O/p : main
+
+
+Variable pattern matching :
+=========================
+
+val y = 7
+def varibalematch(y:Any):String = {
+y match {
+  case a => s"i have got $a"
+  //case _ => "OK"
+} 
+}
+//this code is unreachable,as it wont go to default case
+//What ever we are giving goes to a and thats the default case
+
+import Math.{PI,E}
+def variableMatchConflict(x:Any):String = {
+  x match {
+    case PI => s"GOt PI $PI" //PI - is constantc//ase pi => s"got $pi"(this is varible)
+    case E => s"GOt PI $E"
+    case _ => s"got default"
+  }
+}
+
+variableMatchConflict(PI)
+//res1: String = GOt PI 3.141592653589793
+
+variableMatchConflict("HI")
+//res1: String = got default
